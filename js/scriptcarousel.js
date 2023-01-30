4c4b41284314b70888d5e6d2e6dffc0645ebc316
SzaboRichard01 <ricsi5822@gmail.com>
2023-01-30 09:14:00 +0100

    carousel2

--- js/scriptcarousel.js
+++ js/scriptcarousel.js
@@ -2,17 +2,34 @@ let slideIndex = 0;
 showSlides();
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides1(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides1(slideIndex = n);
 }
 
-function showSlides(n) {
+function showSlides1(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
   let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
