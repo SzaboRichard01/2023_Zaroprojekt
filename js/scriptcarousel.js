fbe96550b9bfcb377c9c776a2e57ba6739b3bed2
SzaboRichard01 <ricsi5822@gmail.com>
2023-01-30 09:02:12 +0100

    carousel

--- js/scriptcarousel.js
+++ js/scriptcarousel.js
@@ -1,5 +1,5 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+showSlides();
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -13,21 +13,15 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1
-}    
-  if (n < 1) {
-    slideIndex = slides.length
-}
-
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-
-
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000);
 }
