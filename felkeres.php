474dc480dd424ceef7d9c64fe8de71c17968c8e0
SzaboRichard01 <ricsi5822@gmail.com>
2023-02-10 11:58:09 +0100

    edzo-kliens lista es adataik

---
+++ felkeres.php
@@ -0,0 +1,9 @@
+<?php
+    require "kapcsolat.php";
+    session_start();
+
+    $valasztott = mysqli_real_escape_string($dbconn, $_GET['felhasznalo_id']);
+    $felh_id = $felh_id = $_SESSION['felh_id'];
+
+    $sql = mysqli_query($dbconn, "INSERT INTO `edzo-felhasznalo` (edzo_az, kliens_az) VALUES ('{$valasztott}', '{$felh_id}')");
+?>
