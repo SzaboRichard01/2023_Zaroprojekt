a1d920b3be919283e68a9ec37091ba7c00f51711
SzaboRichard01 <ricsi5822@gmail.com>
2023-02-22 07:56:53 +0100

    fix

--- app/felkeres.php
+++ app/felkeres.php
@@ -6,5 +6,5 @@
     $felh_id = $felh_id = $_SESSION['felh_id'];
 
     $sql = mysqli_query($dbconn, "INSERT INTO `edzo-felhasznalo` (edzo_az, kliens_az) VALUES ('{$felh_id}', '{$valasztott}')");
-    header("Location: kliens.php");
+    //header("Location: kliens.php");
 ?>
