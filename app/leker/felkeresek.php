ec56b99b8b33976b642ee42957c0c2aacff6ec27
SzaboRichard01 <ricsi5822@gmail.com>
2023-02-22 08:32:57 +0100

    navbar fix

--- app/leker/felkeresek.php
+++ app/leker/felkeresek.php
@@ -2,8 +2,6 @@
 $felh_id = $_SESSION['felh_id'];
 $pTipus = $_SESSION['p_tipus'];
 
-
-
 $sql = "SELECT `edzo-felhasznalo_id`, edzo_az, kliens_az, felkeres_datuma, elfogadva,
         felhasznalok.felhasznalo_id, felhasznalok.vnev, felhasznalok.knev, felhasznalok.kep
         FROM `edzo-felhasznalo`
@@ -14,19 +12,18 @@ $eredmeny = mysqli_query($dbconn, $sql);
 while($sor = mysqli_fetch_assoc($eredmeny)){
     $edzoaz = $sor['edzo_az'];
 
+    $felkeres = "";
 
-$felkeres = "";
+        $felkeres .= "<div class=\"felkeres\">
+        <a href=\"profilAdatok.php?felhasznalo_id=" .$sor['felhasznalo_id']." \">
+        <div class=\"felh\">
+        <div class=\"pkep pkep-meret\"><img src=\"../pics/profile/" .$sor['kep']. "\"></div>
+        <p>{$sor['vnev']} {$sor['knev']}</p>\n
+        </div></a>
+        <button onclick=\"location.href='muveletek/fMegerosites.php?edzo_az={$edzoaz}'\">Megerősítés</button>
+        <button>Elutasítás</button>
+        </div>";
 
-    $felkeres .= "<div class=\"felkeres\">
-    <a href=\"profilAdatok.php?felhasznalo_id=" .$sor['felhasznalo_id']." \">
-    <div class=\"felh\">
-    <div class=\"pkep pkep-meret\"><img src=\"../pics/profile/" .$sor['kep']. "\"></div>
-    <p>{$sor['vnev']} {$sor['knev']}</p>\n
-    </div></a>
-    <button onclick=\"location.href='muveletek/fMegerosites.php?edzo_az={$edzoaz}'\">Megerősítés</button>
-    <button>Elutasítás</button>
-    </div>";
-
-print($felkeres);
+    print($felkeres);
 }
 ?>
