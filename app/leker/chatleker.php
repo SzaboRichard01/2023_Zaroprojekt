c7580b36ea0d1702d55159b25765f70e74690d23
SzaboRichard01 <ricsi5822@gmail.com>
2023-03-06 11:49:54 +0100

    chat lekerdezes fix

--- app/leker/chatleker.php
+++ app/leker/chatleker.php
@@ -6,23 +6,22 @@ session_start();
     if (isset($_SESSION['chataz'])) {
         $fogadoAz = $_SESSION['chataz'];
         
-        $sqlKim = mysqli_query($dbconn, "SELECT kimeno_id, bejovo_id, uzenet FROM uzenet WHERE kimeno_id = {$_SESSION['felh_id']} AND bejovo_id = {$fogadoAz}");
+        $sqlKim = mysqli_query($dbconn, "SELECT kimeno_id, bejovo_id, uzenet FROM uzenet WHERE kimeno_id = {$_SESSION['felh_id']} AND bejovo_id = {$fogadoAz}
+        OR bejovo_id = {$_SESSION['felh_id']} AND kimeno_id = {$fogadoAz}");
         $kiiras = "";
         while($sorKim = mysqli_fetch_assoc($sqlKim)){
-            $kiiras .= "<div class=\"kimenoUz\">
+            if($sorKim['kimeno_id'] == $_SESSION['felh_id']){
+                $kiiras .= "<div class=\"kimenoUz\">
                 <p>{$sorKim['uzenet']}</p>
-            </div>";
-            
-        }
-        //Kimeno vége
-
-        //Bejövő
-
-        $sqlBej = mysqli_query($dbconn, "SELECT kimeno_id, bejovo_id, uzenet FROM uzenet WHERE bejovo_id = {$_SESSION['felh_id']} AND kimeno_id = {$fogadoAz}");
-        while ($sorBej = mysqli_fetch_assoc($sqlBej)) {
-            $kiiras .= "<div class=\"bejovoUz\">
-                        <p>{$sorBej['uzenet']}</p>
+                </div>";
+            }
+            else{
+                $kiiras .= "<div class=\"bejovoUz\">
+                        <p>{$sorKim['uzenet']}</p>
                     </div>";
+            }
+
+            
             
         }
         print $kiiras;
