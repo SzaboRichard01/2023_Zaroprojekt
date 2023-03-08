2183199355bca7ec37ed4c4cf0e29459e8b4a06c
SzaboRichard01 <ricsi5822@gmail.com>
2023-03-08 11:18:27 +0100

    favicon, css vissza

--- kliens_reg.php
+++ kliens_reg.php
@@ -80,6 +80,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="pics/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/reg.css">
     <title>Kliens Regisztráció</title>
